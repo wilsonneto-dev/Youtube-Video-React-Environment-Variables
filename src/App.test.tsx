95986bd6ea206a8099d892256111426d00830eb3
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-12-02 01:14:34 -0300

    final da aula

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(process.env.REACT_APP_API_URL).toBe("http://api.com.br/");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          App Title: { process.env.REACT_APP_TITLE02 } <br />
+          API URL: { process.env.REACT_APP_API_URL } <br />
         </p>
         <a
           className="App-link"
